24614fe750271529d7c00d4c47cc14462e498ef5
Naprasno <code.94@mail.ru>
2021-12-04 15:34:34 +0300

    first commit

---
+++ logo.inc.php
@@ -0,0 +1,5 @@
+    
+                <div class="logo"> 
+                <img src="img/WD.png" alt="php">
+                </div>                                                 
+        
---
+++ menu.inc.php
@@ -0,0 +1,7 @@
+            <nav>
+                <ul>
+                    <li><a href="#"> Главная </a></li>
+                    <li><a href="#"> Знания </a></li>
+                    <li><a href="#"> Контакты </a></li>
+                </ul>    
+            </nav>
